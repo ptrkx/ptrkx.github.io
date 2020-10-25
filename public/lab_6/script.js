7afbbfee9856885ba4a818e29f8b49c48c115373
ptrkx <70548323+ptrkx@users.noreply.github.com>
2020-10-24 23:38:25 -0400

    lab 6

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,3 @@
-// You may wish to find an effective randomizer function on MDN.
 
 function range(int) {
   const arr = [];
@@ -17,6 +16,12 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -27,10 +32,30 @@ document.body.addEventListener('submit', async (e) => {
     },
     body: JSON.stringify(form)
   })
-    .then((fromServer) => fromServer.json())
+    .then((fromServer) => fromServer.json());
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+        if(document.querySelector('.flex-inner')){
+        document.querySelector('.flex-inner').remove();
+        }
+        
+        const countrieslist = range(10);
+        const countriesliist = countries.map(() => {
+          const num = getRandomIntInclusive(0,243);
+          return fromServer[num];
+        });
+
+        const revcountrieslist = countriesliist.sortFunction((a, b) => sortFunction(a, b, 'name'));
+        const ol = document.createElement('ol');
+        clist.className = 'flex-inner';
+        $('form').prepend(ol);
+
+        revcountrieslist.forEach((el, i) => {
+          const country = document.createElement('li');
+          $(country).append('<input type = "checkbox" value = ${el.code} id = ${el.code} />');
+          $(country).append('<label for = ${el.code}> ${el.name} </label>');
+          $(clist).append(country);
+      })
+
     })
     .catch((err) => console.log(err));
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import countries from './public/lab_6/countries.js';
 
 dotenv.config();
 
@@ -22,11 +23,10 @@ app.use((req, res, next) => {
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post((req, res) => {
+    res.json('countries');
     console.log('POST request detected');
-    res.send("hello world");
     console.log('Form data in res.body', req.body);
   });
 
